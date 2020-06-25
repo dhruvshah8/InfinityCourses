ffbe90c8786f1313ecd693489b2d0ad2cc3bbfcc
dhruvshah8 <dhruvshah5784@gmail.com>
2020-06-25 16:51:28 -0400

    Initial Commit

---
+++ InfinityCourses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfinityCourses
+//
+//  Created by Dhruv Shah on 2020-06-25.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
