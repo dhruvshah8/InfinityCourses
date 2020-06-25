9df3bc59c9c8c11b6c69f111eda6cc9e7fa5e3b9
dhruvshah8 <dhruvshah5784@gmail.com>
2020-06-25 16:57:24 -0400

    Initial Assets and Content View

---
+++ InfinityCourses/HomeList.swift
@@ -0,0 +1,21 @@
+//
+//  HomeList.swift
+//  InfinityCourses
+//
+//  Created by Dhruv Shah on 2020-06-25.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeList_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeList()
+    }
+}
